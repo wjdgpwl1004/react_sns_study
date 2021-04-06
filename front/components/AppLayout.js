3dc78f17dff7fbf072addf135f36008591bb1167
wjdgpwl1004 <wjdgpwl1004@naver.com>
2021-04-06 22:36:06 +0900

    리덕스 유저, 프로필부분

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,15 +1,35 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Link from 'next/link';
+import PropTypes from 'prop-types';
 import { Col, Input, Menu, Row } from 'antd';
-import LoginForm from "./LoginForm";
-import UserProfile from "./UserProfile";
+import { useSelector } from 'react-redux';
+import { createGlobalStyle } from 'styled-components';
 
-const AppLayout = ({children}) => {
+import LoginForm from './LoginForm';
+import UserProfile from './UserProfile';
+
+const Global = createGlobalStyle`
+  .ant-row {
+    margin-right: 0 !important;
+    margin-left: 0 !important;
+  }
+  
+  .ant-col:first-child {
+      padding-left: 0 !important;
+  }
+  
+  .ant-col:last-child {
+    padding-right: 0 !important;
+  }
+`;
+
+const AppLayout = ({ children }) => {
+    const { isLoggedIn } = useSelector(state => state.user);
     return (
         <div>
+            <Global />
             <Menu mode="horizontal">
-                <Menu.Item key="home"><Link href="/"><a>홈</a></Link></Menu.Item>
+                <Menu.Item key="home"><Link href="/"><a>노드버드</a></Link></Menu.Item>
                 <Menu.Item key="profile"><Link href="/profile"><a>프로필</a></Link></Menu.Item>
                 <Menu.Item key="mail">
                     <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
@@ -17,19 +37,20 @@ const AppLayout = ({children}) => {
             </Menu>
             <Row gutter={8}>
                 <Col xs={24} md={6}>
-                    <LoginForm />
-                    {/*<UserProfile/>*/}
+                    {isLoggedIn
+                        ? <UserProfile />
+                        : <LoginForm />}
                 </Col>
                 <Col xs={24} md={12}>
                     {children}
                 </Col>
                 <Col xs={24} md={6}>
-                    <a href="#" target="_blank" rel="noreferrer noopener">Made by hyeji</a>
+                    <a href="https://www.zerocho.com" target="_blank" rel="noreferrer noopener">Made by hyeji</a>
                 </Col>
             </Row>
         </div>
     );
-}
+};
 
 AppLayout.propTypes = {
     children: PropTypes.node.isRequired,
--- front/components/LoginForm.js
+++ front/components/LoginForm.js
@@ -1,20 +1,26 @@
 import React, { useCallback } from 'react';
 import { Button, Form, Input } from 'antd';
 import Link from 'next/link';
+import { useDispatch } from 'react-redux';
 
 import useInput from '../hooks/useInput';
+import { loginAction } from '../reducers/user';
 
 const LoginForm = () => {
     const [id, onChangeId] = useInput('');
     const [password, onChangePassword] = useInput('');
+    const dispatch = useDispatch();
+
     const onSubmitForm = useCallback(() => {
-        console.log({
-            id, password,
-        });
+        dispatch(loginAction({
+            id,
+            password,
+        }));
     }, [id, password]);
 
     return (
         <Form onFinish={onSubmitForm} style={{ padding: '10px' }}>
+
             <div>
                 <label htmlFor="user-id">아이디</label>
                 <br />

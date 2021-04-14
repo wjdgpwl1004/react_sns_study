cd6fc2a3a0d1622cef70af9c2098cbe784c7c705
wjdgpwl1004 <wjdgpwl1004@naver.com>
2021-04-14 17:59:07 +0900

    redux-saga 생성

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import PropTypes from 'prop-types';
+import withReduxSaga from 'next-redux-saga';
 import 'antd/dist/antd.css';
 
 import wrapper from '../store/configureStore';
@@ -20,4 +21,8 @@ App.propTypes = {
     Component: PropTypes.elementType.isRequired,
 };
 
-export default wrapper.withRedux(App);
+export function reportWebVitals(metric) {
+    console.log(metric);
+}
+
+export default wrapper.withRedux(withReduxSaga(App));
